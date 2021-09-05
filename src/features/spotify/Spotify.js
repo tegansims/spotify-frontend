111641e82c75328f3b419d0d4eac87c7aa310b15
Tegan Sims <tegan@engaging.tech>
2021-09-05 20:11:50 +0100

    search page functionality & styling

--- src/features/spotify/Spotify.js
+++ src/features/spotify/Spotify.js
@@ -1,26 +1,66 @@
 import React, { useState } from "react";
-import "../../App.css";
+import {
+  Flex, Button, Text, Input, Link
+} from "theme-ui";
+import { UploadIcon } from "@heroicons/react/solid";
 
 const Spotify = () => {
-  const [data, setData] = useState(null);
+  const [searchedTracks, setSearchedTracks] = useState(null);
+  const [searchTracksTerm, setSearchTracksTerm] = useState(null);
+  const [url, setUrl] = useState();
 
-  const handleClick = () => fetch("/api")
-  .then((res) => res.json())
-  .then((data) => setData(data.message));
-
-  const handleArtistAlbums = () => fetch("/artist/43ZHCT0cAZBISjO8DG9PnE")
-  .then((res) => res.json())
-  .then((data) => { console.log(data.albums); setData(data.albums.href); });
+  const handleSearchTracks = () => fetch(`/tracks/search/${searchTracksTerm}`)
+    .then((res) => res.json())
+    .then((data) => {
+      setSearchedTracks(data.searchedTracks.tracks.items);
+    });
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>{!data ? "Loading..." : data}</p>
-        {/* <a href="/login" className="btn btn-primary">Log in with Spotify</a> */}
-        <button onClick={handleClick}>test alert</button>
-        <button onClick={handleArtistAlbums}>Get Elvis's albums</button>
-      </header>
-    </div>
+    <Flex
+      sx={{
+        bg: "background",
+        height: "100%",
+        width: "100%",
+        p: 4,
+        alignItems: "center",
+        justifyContent: "center",
+        flexDirection: "column",
+      }}
+    >
+      <Flex sx={{ mb: 20 }}>
+        <Input
+          placeholder="Enter your track"
+          value={searchTracksTerm}
+          onChange={(e) => setSearchTracksTerm(e.target.value)}
+        />
+        <Button sx={{ m: 10, width: "100%" }} onClick={handleSearchTracks} disabled={!searchTracksTerm}>Search tracks</Button>
+      </Flex>
+
+      {url && <iframe src={`https://open.spotify.com/embed/track/${url}`} title={url} width="300" height="380" frameBorder="0" allowtransparency="true" allow="encrypted-media" />}
+
+      {searchedTracks?.length === 0 && <Text onClick={() => setSearchTracksTerm("")}>No results! Please try again</Text>}
+
+      {searchedTracks?.map((track) => (
+        <Flex key={track.uri} sx={{ mt: 10, alignItems: "center" }}>
+          <Text sx={{ color: "primary" }} onClick={() => setUrl(track.id)}>
+            {track.name}
+          </Text>
+          <Text>{" - "}</Text>
+          <Flex sx={{ flexDirection: "row", flexWrap: "wrap", alignItems: "center" }}>
+            {track.artists?.map((artist, index) => (
+              <Text key={artist.uri}>
+                {artist?.name}
+                {index + 1 < track.artists.length ? "·" : ""}
+              </Text>
+            ))}
+            <Link href={track.external_urls.spotify} target="_blank" sx={{ color: "primary" }}>
+              <UploadIcon height={18} width={18} style={{ paddingLeft: 4 }} />
+            </Link>
+          </Flex>
+
+        </Flex>
+      ))}
+    </Flex>
   );
 };
 
